c1006d02ba53433ae5e5c78d2f3d08522a246d4b
Tarlan <tarlanru@code.edu.az>
2018-06-02 22:48:49 +0300

    RESTORAN

---
+++ Restoran/MainForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Restoran
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.price_box = new System.Windows.Forms.TextBox();
+            this.cmbx_name = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbx_category = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(352, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login As Admin";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.price_box);
+            this.groupBox1.Controls.Add(this.cmbx_name);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbx_category);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(90, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 334);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Order";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Count";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 241);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(104, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Order";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Price";
+            // 
+            // price_box
+            // 
+            this.price_box.Location = new System.Drawing.Point(34, 177);
+            this.price_box.Name = "price_box";
+            this.price_box.ReadOnly = true;
+            this.price_box.Size = new System.Drawing.Size(268, 20);
+            this.price_box.TabIndex = 4;
+            // 
+            // cmbx_name
+            // 
+            this.cmbx_name.FormattingEnabled = true;
+            this.cmbx_name.Location = new System.Drawing.Point(34, 109);
+            this.cmbx_name.Name = "cmbx_name";
+            this.cmbx_name.Size = new System.Drawing.Size(268, 21);
+            this.cmbx_name.TabIndex = 3;
+            this.cmbx_name.SelectedIndexChanged += new System.EventHandler(this.cmbx_name_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // cmbx_category
+            // 
+            this.cmbx_category.FormattingEnabled = true;
+            this.cmbx_category.Location = new System.Drawing.Point(34, 46);
+            this.cmbx_category.Name = "cmbx_category";
+            this.cmbx_category.Size = new System.Drawing.Size(268, 21);
+            this.cmbx_category.TabIndex = 1;
+            this.cmbx_category.SelectedIndexChanged += new System.EventHandler(this.cmbx_category_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Category";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox price_box;
+        private System.Windows.Forms.ComboBox cmbx_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbx_category;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Restoran/MainForm.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Restoran
+{
+    public partial class MainForm : Form
+    {
+        ArrayList products = new ArrayList();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            products.AddRange(new Product[]{
+                  new Product{
+                    Name = "Kabab",
+                    Price = 4,
+                    CountOf = 100,
+                    ProductCategory = ProductCategory.MainFoods
+
+                },
+                  new Product {
+                Name = "Sac",
+                Price = 10,
+                CountOf = 100,
+                ProductCategory = ProductCategory.MainFoods
+
+                },
+                  new Product {
+                Name = "Dolma",
+                Price = 5,
+                CountOf = 100,
+                ProductCategory = ProductCategory.MainFoods
+
+                },
+                      new Product {
+                Name = "Ezo Gelin",
+                Price = 5,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Soups
+
+                },
+                            new Product {
+                Name = "Tomat Supu",
+                Price = 2,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Soups
+
+                },
+                                new Product {
+                Name = "Paytaxt Salati",
+                Price = 1.50,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Salats
+
+                },
+                                 new Product {
+                Name = "Mimoza Salati",
+                Price = 0.50,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Salats
+
+                },
+                                     new Product {
+                Name = "Mimoza Salati",
+                Price = 2.50,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Salats
+
+                },
+                                       new Product {
+                Name = "Ayran",
+                Price = 0.50,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Drinks
+
+                },
+                                       new Product {
+                Name = "Cola",
+                Price = 1.00,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Drinks
+
+                },
+                                       new Product {
+                Name = "Fanta",
+                Price = 1.00,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Drinks
+
+                },
+                                       new Product {
+                Name = "Sutlac",
+                Price = 2.50,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Deserts
+
+                },
+                                       new Product {
+                Name = "Paxlava",
+                Price = 3.50,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Deserts
+
+                },
+                                       new Product {
+                Name = "Tort",
+                Price = 5.50,
+                CountOf = 100,
+                ProductCategory = ProductCategory.Deserts
+
+                },
+            });
+
+      
+            cmbx_category.DataSource = Enum.GetValues(typeof(ProductCategory));
+        }
+
+      
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbx_category_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = (sender as ComboBox).SelectedItem.ToString();
+            ProductCategory foodsOfselectingCategory = (ProductCategory)Enum.Parse(typeof(ProductCategory), selectedItem);
+            cmbx_name.Items.Clear();
+            foreach (Product item in products)
+            {
+                if(item.ProductCategory == foodsOfselectingCategory)
+                {
+                    cmbx_name.Items.Add(item.Name);
+                }
+            }
+        }
+
+        private void cmbx_name_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string SelectedValue = (sender as ComboBox).SelectedItem.ToString();
+            foreach (Product item in products)
+            {
+                if(item.Name == SelectedValue)
+                {
+                    price_box.Text = item.Price +  " " + "AZN";
+                }
+            }
+            
+        }
+    }
+}
